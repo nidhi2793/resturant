7b5815595fe19296f2118c0f347c393506e70797
nidhi2793 <MY_NAME@example.com>
2023-07-25 23:20:12 +0530

    Form

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -11,7 +11,9 @@ function MealItem(props) {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 }
